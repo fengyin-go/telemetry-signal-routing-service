cfdf9f76d362cd6a532ae3f151eb5cf7bb66db14
fengyin-go <1621476774@qq.com>
2026-08-26 21:49:09 +0800

    fix+test: 49-file-storage【30】-002

--- internal/state02/state.go
+++ internal/state02/state.go
@@ -8,7 +8,7 @@ import (
 type Sink struct{ calls atomic.Int32 }
 
 func (s *Sink) Deliver(ctx context.Context, signal string) error {
-	if err := context.Background().Err(); err != nil {
+	if err := ctx.Err(); err != nil {
 		return err
 	}
 	s.calls.Add(1)
